7ac19d0f4d0852397fb6dc4fe798694af3ed3a96
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2020-05-23 21:19:25 +0100

    Update client.py

--- api/client.py
+++ api/client.py
@@ -190,7 +190,7 @@ class Client():
 		}
 		self.s = requests.Session()
 		self.s.headers.update({
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+			'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
 						  "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/"
 						  "81.0.4044.138 Safari/537.36"
 		})
